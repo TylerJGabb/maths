a0e7ed8dc106ff5fd910f74c27cc434303753014
Tyler Gabb <Tyler Gabb>
2017-05-12 21:24:30 -0700

    Last update of the night

--- Primes/largest_prime_factor.py
+++ Primes/largest_prime_factor.py
@@ -4,7 +4,6 @@ import time as T
 ##What is the largest prime factor of a number n
 ##Prime numbers, if they are more than 1 digit, end in 
 ## 1,3,7,9
-file = open('LPF.csv','w')
 
 def isprime1(n):
     if n < 10: 
@@ -21,47 +20,11 @@ def isprime1(n):
     return True
  
 
-#Trying to find a pattern between mod and digit. I doubt it
-#is necessary to test ALL digits, there must be a way
-#to rule some out 
-
-#This program spends most of its time just iterating
-#through the while loop, and decrementing digit by 1
-#there has to be another way to decrement quickly, and rule out
-#digits
-
-#Notice that the code only prints the digit being tested
-#and its mod with n every 10-million loops. It is still slow....
-
-
-#Perhaps there is a way to predict whether digit will divide n,
-#ahead of time. Perhaps consider some proabability model
-#to immediately rule out those which have 0 percent
-#probability of dividing evenly. 
-
-def find_LPF(n):
-    if isprime1(n):
-        print(n,'IS PRIME')
-        return -1
-    digit = n//2 #integer division to find floor of half
-    loops = 0
-    mod = None
-    while digit > 1:
-        if loops%10000000 == 0: print(digit,':',mod)
-        mod = n%digit
-        if mod == 0:
-            print('-----',digit,'divides',n,'evenly ----')
-            print('testing_primality of',digit)
-            tic = T.time()
-            is_prime = isprime1(digit)
-            toc = T.time()
-            time = toc-tic
-            if time > 1: print('took',time,'seconds')
-            if is_prime: return digit
-            input('enter to continue')
-            print('resuming')
-        digit -= 1
-        loops += 1
+def LPF(n):
+    #Need to start with 2, and move up to find next prime factors
+    #Then when done factoring, return the largest factor....
+    #That's what makes this take a while. Perhaps consider a recursive function
+    
         
 #DEVELOPMENT TOOL       
 def test_behavior():
@@ -71,15 +34,7 @@ def test_behavior():
         print(dig,':',n%dig)
         dig -= 1
         
-        
-#Returns the unique prime factorization of a given number n
-def factorize(n):
-    divisor = 2
-    while n > 0:
-        rem = n%divisor
-        if rem == 0:
-            n = n//divisor
-            
+
 
 #Generates THE NEXT PRIME after n
 def getNextPrime(n):
@@ -90,6 +45,14 @@ def getNextPrime(n):
         if isprime1(dig): return dig #This is a lie here, this is actually very
         #complex loop
         dig += 2
+        
+    
+    
+    
+    
+    
+    
+    
     
         
 
